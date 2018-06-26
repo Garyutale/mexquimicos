1eddb43ce1d147b61ecf8292e446fbec52d8a506
Garyutale <paab_003@hotmail.com>
2018-06-25 19:57:24 -0500

    hola_chio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    hola chio
 
                     You are logged in!
                 </div>
